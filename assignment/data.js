95e958eda53c358f89eb1c328d468b12acda282e
fahadshah1ff <fahadshah1ff@gmail.com>
2026-02-25 07:08:56 +0000

    data table

---
+++ assignment/data.js
@@ -0,0 +1,7 @@
+const rollno=23;
+let name="Syed Afzal shah";
+let age =24;
+let gender = "male";
+let batch="Python Web";
+
+console.table(["rollno " + rollno, "name " + name, "age " + age, "Gender " + gender, "batch " + batch, ])
