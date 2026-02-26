a41b35ba1293a322230b58d7e2180505b93b7f4c
fahadshah1ff <fahadshah1ff@gmail.com>
2026-02-26 06:47:41 +0000

    table

--- assignment/first.js
+++ assignment/first.js
@@ -1,5 +1,6 @@
-const =3565;
+const id =3565;
 let name="Fahad";
 let age= 23;
 let batch= 2021;
-console.table(["id " + id, "Fahad", "age "+ age, "batch"+ batch])
+let country="Pakistan"
+console.table(["id " + id, "Fahad", "age "+ age, "batch"+ batch, "Country " +country,])
---
+++ assignment/task1.js
@@ -0,0 +1,8 @@
+let studentName="Fahad";
+let totalMarks=800;
+let obtainedMarks=755;
+ let percentage=(obtainedMarks/ totalMarks)* 100;
+ console.log("student ",studentName);
+ console.log("total Marks",totalMarks);
+ console.log("otained Marks", obtainedMarks);
+ console.log("percentage",percentage + "%");
