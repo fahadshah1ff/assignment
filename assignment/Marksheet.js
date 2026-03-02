9e79e9e123c906edff2af45843908382224c42c2
fahadshah1ff <fahadshah1ff@gmail.com>
2026-03-02 06:41:15 +0000

    marksheet

---
+++ assignment/Marksheet.js
@@ -0,0 +1,27 @@
+let name="Syed Fahad Shah";
+let fathername="Syed Dhani dino Shah";
+let rollno=210;
+
+// Subject 
+let Sindhi=55;
+let urdu=54;
+let English=60;
+let islamiat=48;
+let physics=68;
+let chemistry=59;
+let totalMarks=( Sindhi+ urdu+ English+ islamiat+ physics+ chemistry)
+
+let percentage=(totalMarks / 600) * 100;
+let grade="A"
+console.log("name "+ name);
+console.log("fathername "+ fathername);
+console.log("rollno "+ rollno);
+console.log("Sindhi "+ Sindhi);
+console.log("urdu "+ urdu);
+console.log("Englist "+ English);
+console.log("islamiat "+ islamiat);
+console.log("physics "+ physics)
+console.log("chemistry "+ chemistry)
+console.log("TotalMarks : ", totalMarks )
+console.log("Percentage : ", percentage)
+console.log("grade "+ grade);
